418aee750afab43f910ae7c35d323f2843732d1f
narimenmokrani1 <narimenmokrani@gmail.com>
2021-12-07 16:58:58 -0600

    addded cached element referneces and events listeners

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log("check ")
+console.log('check from app');
+
+/*----- constants -----*/
+
+/*----- app's state (variables) -----*/
+let matchedCards = [];
+
+/*----- cached element references -----*/
+const resetBtn = document.querySelector('#reset');
+const DarkModeBtn = document.querySelector('#dark-mode');
+const sneakPeekBtn = document.querySelector('#sneak-peek');
+const gameContainer = document.querySelector('.game-container');
+const cards = document.querySelector('.card');
+const h2Counter = document.querySelector('h2');
+
+/*----- event listeners -----*/
+resetBtn.addEventListener('click', init);
+cards.addEventListener('click', flipCard);
+DarkModeBtn.addEventListener('click', darkTheme);
+sneakPeekBtn.addEventListener('click', checkCards);
+
+/*----- functions -----*/
